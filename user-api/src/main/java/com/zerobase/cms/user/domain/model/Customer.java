4b56f124d57986ef3d81bd3d0703537d4d4b37af
hj <kwonfelix93@gmail.com>
2024-02-26 21:59:33 +0900

    feat : [002-SIGN-UP] 커스터머(고객)에 대한 Signup Service 구현

---
+++ user-api/src/main/java/com/zerobase/cms/user/domain/model/Customer.java
@@ -0,0 +1,47 @@
+package com.zerobase.cms.user.domain.model;
+
+import com.zerobase.cms.user.domain.SignUpForm;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.envers.AuditOverride;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Locale;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@AuditOverride(forClass = BaseEntity.class) // Customer 테이블이 업데이트 될 때마다 자동으로 createdate 와 lastmodifieddate 가 변경됨
+public class Customer extends BaseEntity {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String name;
+    private String password;
+    private String phone;
+    private LocalDate birth;
+
+    private LocalDateTime verifyExpiredAt;
+    private String verificationCode;
+    private boolean verify;
+
+    public static Customer from(SignUpForm form) {
+        return Customer.builder()
+                .email(form.getEmail().toLowerCase(Locale.ROOT))
+                .password(form.getPassword())
+                .name(form.getName())
+                .phone(form.getPhone())
+                .birth(form.getBirth())
+                .verify(false)
+                .build();
+    }
+}
---
+++ user-api/src/test/java/com/zerobase/cms/user/service/SignUpCustomerServiceTest.java
@@ -0,0 +1,36 @@
+package com.zerobase.cms.user.service;
+
+import com.zerobase.cms.user.domain.SignUpForm;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SignUpCustomerServiceTest {
+
+    @Autowired
+    private SignUpCustomerService service;
+
+    @Test
+    void signUp() {
+        // given
+        // when
+        SignUpForm form = SignUpForm.builder()
+                .name("name")
+                .birth(LocalDate.now())
+                .email("abc@gmail.com")
+                .password("1")
+                .phone("01000000000")
+                .build();
+
+        // then
+        Assert.isTrue(service.signUp(form).getId()!=null);
+
+    }
+
+}
